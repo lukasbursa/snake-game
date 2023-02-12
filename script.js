51f9d52c1cb580b15956357dcc734044ea74609c
lukasbursa <bursalukas@gmail.com>
2023-02-12 14:13:57 +0100

    snake hotov

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 // Settings
 
 let snakeSize = 20 // Size of one square of snake
-let playgroundSizeX = 500 // Width of playground
-let playgroundSizeY = 500 // Height of playground
+let playgroundSizeX = 400 // Width of playground
+let playgroundSizeY = 400 // Height of playground
+let gameSpeed = 100
 let playgroundColor = "rgb(53, 127, 247)"
 let snakeColor = "black"
 let foodColor = "red"
@@ -17,10 +18,9 @@ let startY = Math.floor(playgroundSizeY /snakeSize / 2) * snakeSize
 let canvasSelect = document.querySelector("#playground")
 let canvas = canvasSelect.getContext("2d")
 
-// Create default snake Array
+// Create default snake
 
-let snake = {
-    position: [{
+const defaultSnakePosition = [{
         x: startX,
         y: startY
     },{
@@ -29,69 +29,52 @@ let snake = {
     },{
         x: startX - (snakeSize * 2),
         y: startY
-    }],
-    direction: "right",
-    deletedPart: {
-        x: startX - (snakeSize * 2),
-        y: startY
-    },
+    }]
+const defaultSnakeDirection = "right"
+const defaultSnakeDeletedPart = {
+    x: startX - (snakeSize * 2),
+    y: startY
+}
 
-// Fonctions for mooving snake | if ateFood is true, make snake longer
 
-    moveRight: function(ateFood){
-        canvas.fillStyle = playgroundColor
-        canvas.fillRect(this.deletedPart.x, this.deletedPart.y, snakeSize, snakeSize); 
-        !ateFood ? this.position.pop(): "";    
-        this.deletedPart = this.position[this.position.length - 1]
-        this.position.unshift({
-            x: this.position[0].x + snakeSize,
-            y: this.position[0].y
-        })
-        this.position.forEach(snakePart => {
-            canvas.fillStyle = snakeColor
-            canvas.fillRect(snakePart.x, snakePart.y, snakeSize, snakeSize);
-        })
-    },
+let snake = {
+    position: JSON.parse(JSON.stringify(defaultSnakePosition)),
+    direction: defaultSnakeDirection,
+    deletedPart: defaultSnakeDeletedPart,
 
-    moveLeft: function(ateFood){
+// Fonction for mooving snake | if ateFood is true, make snake longer
+
+    move: function(ateFood, direction){
         canvas.fillStyle = playgroundColor
         canvas.fillRect(this.deletedPart.x, this.deletedPart.y, snakeSize, snakeSize);
         !ateFood ? this.position.pop() : "" ;
         this.deletedPart = this.position[this.position.length - 1]
-        this.position.unshift({
-            x: this.position[0].x - snakeSize,
-            y: this.position[0].y
-        })
-        this.position.forEach(snakePart => {
-            canvas.fillStyle = snakeColor
-            canvas.fillRect(snakePart.x, snakePart.y, snakeSize, snakeSize);
-        })
-    },
-
-    moveUp: function(ateFood){
-        canvas.fillStyle = playgroundColor
-        canvas.fillRect(this.deletedPart.x, this.deletedPart.y, snakeSize, snakeSize);
-        !ateFood ? this.position.pop() : "" ;
-        this.deletedPart = this.position[this.position.length - 1]
-        this.position.unshift({
-            x: this.position[0].x,
-            y: this.position[0].y - snakeSize
-        })
-        this.position.forEach(snakePart => {
-            canvas.fillStyle = snakeColor
-            canvas.fillRect(snakePart.x, snakePart.y, snakeSize, snakeSize);
-        })
-    },
-
-    moveDown: function(ateFood){
-        canvas.fillStyle = playgroundColor
-        canvas.fillRect(this.deletedPart.x, this.deletedPart.y, snakeSize, snakeSize);
-        !ateFood ? this.position.pop() : "" ;
-        this.deletedPart = this.position[this.position.length - 1]
-        this.position.unshift({
-            x: this.position[0].x,
-            y: this.position[0].y + snakeSize
-        })
+        switch(direction){
+            case "right":
+                this.position.unshift({
+                    x: this.position[0].x + snakeSize,
+                    y: this.position[0].y
+                })
+            break;
+            case "left":
+                this.position.unshift({
+                    x: this.position[0].x - snakeSize,
+                    y: this.position[0].y
+                })
+            break;
+            case "up":
+                this.position.unshift({
+                    x: this.position[0].x,
+                    y: this.position[0].y - snakeSize
+                })
+            break;
+            case "down":
+                this.position.unshift({
+                    x: this.position[0].x,
+                    y: this.position[0].y + snakeSize
+                })
+            break;
+        }
         this.position.forEach(snakePart => {
             canvas.fillStyle = snakeColor
             canvas.fillRect(snakePart.x, snakePart.y, snakeSize, snakeSize);
@@ -103,6 +86,10 @@ let snake = {
 
 let food = {
     position: {
+        x: 0,
+        y: 0
+    },
+    previousPosition: {
         x: 0,
         y: 0
     },
@@ -127,6 +114,7 @@ let food = {
 let game = {
     playing: false,
     score: 0,
+    pressedKey: "",
 
     // Function checks if snake collide with border or himself
     isCollision: function(actualDirection){ 
@@ -147,29 +135,17 @@ let game = {
     },
 
     newGame: function(){
-        this.score = 0
-        
-        snake.position = [{
-            x: startX,
-            y: startY
-        },{
-            x: startX - snakeSize,
-            y: startY
-        },{
-            x: startX - (snakeSize * 2),
-            y: startY
-        }]
-        snake.direction = "right"
-        snake.deletedPart = {
-            x: startX - (snakeSize * 2),
-            y: startY
-        }
-        
+        game.score = 0
+       
+        snake.position = JSON.parse(JSON.stringify(defaultSnakePosition))        
+        snake.direction = defaultSnakeDirection
+        snake.deletedPart = defaultSnakeDeletedPart
 
         canvas.fillStyle = playgroundColor
         canvas.fillRect(0, 0, playgroundSizeX, playgroundSizeY);
 
-        food.generateNew() // Render first food on playground
+        // Render first food on playground
+        food.generateNew() 
         
         // Render start snake position
         snake.position.forEach(snakePart => {
@@ -179,52 +155,29 @@ let game = {
     },
 
     runGame: function(){
-
-        // Listen keys and change snake direction
-        // document.addEventListener("keydown", event => {
-        // event.preventDefault()
-        // if(event.key == "ArrowRight" && snake.direction != "left"){
-        //     snake.direction = "right"
-        // }else if(event.key == "ArrowLeft" && snake.direction != "right"){
-        //     snake.direction = "left"
-        // }else if(event.key == "ArrowUp" && snake.direction != "down"){
-        //     snake.direction = "up"
-        // }else if(event.key == "ArrowDown" && snake.direction != "up"){
-        //     snake.direction = "down"
-        // }    
-        // })
-
-        let collision = false
-        let ateFood = false
-    
-        if((food.position.x == snake.position[0].x) && (food.position.y == snake.position[0].y)){
-            ateFood = true
-            food.generateNew()
-            game.score++;
-            console.log(game.score)
-        }
-        //console.log(snake.position[0])
-        if(snake.direction == "right"){
-         
-            snake.moveRight(ateFood)
-            collision = game.isCollision(snake.direction)
-            
-        }else if(snake.direction == "left"){
-            snake.moveLeft(ateFood)
-            collision = game.isCollision(snake.direction)
-        }
-        else if(snake.direction == "up"){
-            snake.moveUp(ateFood)
-            collision = game.isCollision(snake.direction)
-        }else if(snake.direction == "down"){
-            snake.moveDown(ateFood)
-            collision = game.isCollision(snake.direction)
+        if(game.pressedKey == "ArrowRight" && snake.direction != "left"){
+            snake.direction = "right"
+        }else if(game.pressedKey == "ArrowLeft" && snake.direction != "right"){
+            snake.direction = "left"
+        }else if(game.pressedKey == "ArrowUp" && snake.direction != "down"){
+            snake.direction = "up"
+        }else if(game.pressedKey == "ArrowDown" && snake.direction != "up"){
+            snake.direction = "down"
         } 
 
-        if(!collision){setTimeout(game.runGame, 500)}else{
-            //alert("Game Over")
+        let ateFood = ((food.previousPosition.x == snake.position[0].x) && (food.previousPosition.y == snake.position[0].y)) ? true : false
+       
+        snake.move(ateFood, snake.direction)
+        
+        if((food.position.x == snake.position[0].x) && (food.position.y == snake.position[0].y)){
+            food.previousPosition = food.position
+            game.score++;
+            food.generateNew()
+            console.log("Score: " + game.score)
+        }
+
+        if(!game.isCollision(snake.direction)){setTimeout(game.runGame, gameSpeed)}else{
             game.playing = false
-            
             game.newGame()
         }
     }
@@ -239,15 +192,7 @@ game.newGame()
 
 document.addEventListener("keydown", event => {
     event.preventDefault()
-        if(event.key == "ArrowRight" && snake.direction != "left"){
-            snake.direction = "right"
-        }else if(event.key == "ArrowLeft" && snake.direction != "right"){
-            snake.direction = "left"
-        }else if(event.key == "ArrowUp" && snake.direction != "down"){
-            snake.direction = "up"
-        }else if(event.key == "ArrowDown" && snake.direction != "up"){
-            snake.direction = "down"
-        } 
+    game.pressedKey = event.key
 
     if((event.key == "ArrowRight" || event.key == "ArrowLeft" || event.key == "ArrowUp" || event.key == "ArrowDown") && game.playing == false){
         game.playing = true
